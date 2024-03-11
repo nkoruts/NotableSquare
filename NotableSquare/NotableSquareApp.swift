8e0d10cf9f0b8a1829898ca5c8bf7ce4a9d07c41
Nikita Koruts <nikitakoruts@gmail.com>
2024-03-11 09:35:51 +0200

    Init project

---
+++ NotableSquare/NotableSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotableSquareApp.swift
+//  NotableSquare
+//
+//  Created by Nikita Koruts on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotableSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
