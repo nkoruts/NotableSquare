2fbd6e1cfb73cacfec796135b850a32be7800691
Nikita Koruts <nikitakoruts@gmail.com>
2024-03-12 11:58:45 +0200

    complete project

--- NotableSquare/ContentView.swift
+++ NotableSquare/ContentView.swift
@@ -8,16 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragOffset: CGSize = .zero
+    
+    private let colors: [Color] = [.white, .pink, .yellow, .black]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: .zero) {
+                ForEach(colors, id: \.self) { color in
+                    Rectangle()
+                        .fill(color)
+                }
+            }
+            DraggableView()
+                .frame(width: 100, height: 100)
+                .offset(dragOffset)
+                .gesture(
+                    DragGesture()
+                        .onChanged(dragOnChange(_:))
+                        .onEnded(dragOnEnd(_:)))
         }
-        .padding()
+        .ignoresSafeArea()
+    }
+    
+    private func dragOnChange(_ value: DragGesture.Value) {
+        dragOffset = value.translation
+    }
+    
+    private func dragOnEnd(_ value: DragGesture.Value) {
+        withAnimation(.spring()) {
+            dragOffset = .zero
+        }
     }
 }
+
+struct DraggableView: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.white)
+            .blendMode(.difference)
+            .overlay(.blendMode(.hue))
+            .overlay(Color.white.blendMode(.overlay))
+            .overlay(Color.black.blendMode(.overlay))
+            .cornerRadius(16)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
